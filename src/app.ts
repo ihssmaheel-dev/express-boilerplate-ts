eabfd255545a659392691f77250914251b834cf6
ismail-hepl <ismail.s@hepl.com>
2024-06-14 12:00:06 +0530

    Feat: Implement the errorHandler middleware in the app.ts file

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import fileUpload from "express-fileupload";
+import errorHandler from "./middlewares/errorHandler";
 
 const app = express();
 
@@ -9,9 +10,12 @@ app.use(express.json());
 
 app.use(cors()); // Enable cors for all routes
 
+app.use(fileUpload());
+
 // Routes
 
 
-app.use(fileUpload());
+// Error Handling
+app.use(errorHandler);
 
 export default app;
