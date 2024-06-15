e499246866e0231d6d7de5dd9588855755b441b1
ismail-hepl <ismail.s@hepl.com>
2024-06-15 10:46:44 +0530

    Feat: Create a getUser and getUsers api to fetch the user from the database

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,32 @@
+import {  Request, Response, NextFunction } from "express";
+import asyncHandler from "express-async-handler";
+import logger from "../logger/logger";
+import UserService from '../services/user.service';
+
+class UserController {
+    private userService: UserService;
+
+    constructor() {
+        this.userService = new UserService();
+    }
+
+    getUsers = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+        logger.info("fetching all users...");
+        const users = await this.userService.getUsers();
+
+        logger.info("users fetched successfully");
+        res.status(200).json({ status: true, users });
+    });
+
+    getUser = asyncHandler(async (req: Request, res: Response,next: NextFunction) => {
+        logger.info(`fetching user for id: ${req.params.id}...`);
+        const user = await this.userService.getUser(req.params.id);
+
+        logger.info("user fetched successfully");
+        res.status(200).json({ status: true, user });
+    });
+
+
+}
+
+export default UserController;
---
+++ src/services/user.service.ts
@@ -0,0 +1,18 @@
+import User from "../models/user.model";
+import CustomError from "../utils/customError";
+
+class UserService {
+    async getUsers() {
+        return await User.find();
+    }
+
+    async getUser(id: string) {
+        const user = await User.findById(id);
+        if(!user) throw new CustomError(404, "User not found");
+
+        return user;
+    }
+
+}
+
+export default UserService;
