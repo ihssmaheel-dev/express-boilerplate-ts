1fd3591ec31181971f818205044570f51c4c4bd3
ismail-hepl <ismail.s@hepl.com>
2024-06-15 11:11:29 +0530

    Feat: Implement the userRoutes in the app file

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import fileUpload from "express-fileupload";
 import errorHandler from "./middlewares/errorHandler";
+import userRoutes from "./routes/user.routes";
 
 const app = express();
 
@@ -13,7 +14,7 @@ app.use(cors()); // Enable cors for all routes
 app.use(fileUpload());
 
 // Routes
-
+app.use("/v1/user", userRoutes);
 
 // Error Handling
 app.use(errorHandler);
