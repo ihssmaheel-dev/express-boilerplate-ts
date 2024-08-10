4f45b4f236a5afd830c0f31c11e4fe7a7d74dfa1
ismail-hepl <ismail.s@hepl.com>
2024-08-10 19:45:10 +0530

    Feat: Handle the error response with the new and enhnaced generic response

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import logger from "../logger/logger";
 import CustomError from "../utils/customError";
-import GenericResponse from "../utils/genericResponse";
+import GenericResponse, { ErrorResponse } from "../utils/genericResponse";
 
 const errorHandler = async (error: Error, req: Request, res: Response, next: NextFunction) => {
     logger.error(error);
@@ -9,7 +9,7 @@ const errorHandler = async (error: Error, req: Request, res: Response, next: Nex
     const message = error instanceof CustomError ? error.message : "Something went wrong";
     const statusCode = (error instanceof CustomError && error.statusCode) || 500;
 
-    res.status(statusCode).json(GenericResponse.error(message));
+    res.status(statusCode).json(ErrorResponse(message));
 }
 
 export default errorHandler;
