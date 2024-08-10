75d4dce2f62db4a51edb785e19a59e98f14a52c5
ismail-hepl <ismail.s@hepl.com>
2024-08-10 19:46:43 +0530

    Feat: Handle the response with the new and enhnaced generic response in the hashing controller

--- src/controllers/hashing.controller.ts
+++ src/controllers/hashing.controller.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import asyncHandler from 'express-async-handler';
 import logger from '../logger/logger';
 import AESGCM from '../services/hashing.service';
-import GenericResponse from '../utils/genericResponse';
+import { SuccessResponse } from '../utils/genericResponse';
 
 class HashingController {
     private hashingService: AESGCM;
@@ -16,7 +16,7 @@ class HashingController {
         const encryptedData = await this.hashingService.encrypt(req.body.text);
 
         logger.info("data encrypted successfully");
-        res.status(200).json(GenericResponse.successWithData(encryptedData));
+        res.status(200).json(SuccessResponse(encryptedData));
     });
 
     decrypt = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
@@ -24,7 +24,7 @@ class HashingController {
         const decryptedData = await this.hashingService.decrypt(req.body.text);
 
         logger.info("data decrypted successfully");
-        res.status(200).json(GenericResponse.successWithData(decryptedData));
+        res.status(200).json(SuccessResponse(decryptedData));
     });
 }
 
