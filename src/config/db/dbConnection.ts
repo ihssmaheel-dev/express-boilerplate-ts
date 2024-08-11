ec2b676cebbf6d87df0c2384b391c0dcf5238dd8
ismail-hepl <ismail.s@hepl.com>
2024-08-11 18:00:03 +0530

    Feat: Implement the createindexes in the dbConnection

--- src/config/db/dbConnection.ts
+++ src/config/db/dbConnection.ts
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 import { dbConfig } from "../config";
+import { createIndexes } from "./createIndexes";
 
 async function connectDB(): Promise<void> {
     try {
         console.log("Connecting to the database...");
         await mongoose.connect(dbConfig.url);
+        await createIndexes();
         console.log("Database connected successfully.")
     } catch (error) {
         console.error("Database connection error: ", error);
