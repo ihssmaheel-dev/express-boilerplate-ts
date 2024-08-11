3df5af39850d29fd82e727db242b71eaab1ad4ce
ismail-hepl <ismail.s@hepl.com>
2024-08-11 18:00:23 +0530

    Feat: Handle the mongoDuplicationError and optimize the errorHandler

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,15 +1,47 @@
 import { Request, Response, NextFunction } from "express";
 import logger from "../config/logger/logger";
 import CustomError from "../utils/customError";
-import GenericResponse, { ErrorResponse } from "../utils/genericResponse";
+import { ErrorResponse } from "../utils/genericResponse";
+import { MongoServerError } from "mongodb";
 
-const errorHandler = async (error: Error, req: Request, res: Response, next: NextFunction) => {
+// Function to format duplicate key errors from MongoDB
+const formatDuplicateKeyError = (error: MongoServerError): { [key: string]: string } => {
+    const duplicateKey = Object.keys(error.keyValue)[0];
+    return { [duplicateKey]: `${duplicateKey.charAt(0).toUpperCase() + duplicateKey.slice(1)} already exists.` };
+};
+
+// Function to classify and format errors
+const classifyError = (error: Error | any): { statusCode: number, message: string, errors: { [key: string]: string } } => {
+    if (error instanceof CustomError) {
+        return {
+            statusCode: error.statusCode,
+            message: "Operational Error",
+            errors: { general: error.message }
+        };
+    }
+
+    if (error.name === 'MongoServerError' && error.code === 11000) {
+        return {
+            statusCode: 409, // Conflict for duplicate key errors
+            message: "Validation Error",
+            errors: formatDuplicateKeyError(error)
+        };
+    }
+
+    return {
+        statusCode: 500, // Internal Server Error for unexpected issues
+        message: "Error",
+        errors: { general: "Something went wrong" }
+    };
+};
+
+// Main error handler function
+const errorHandler = (error: Error | any, req: Request, res: Response, next: NextFunction) => {
     logger.error(error);
 
-    const message = error instanceof CustomError ? error.message : "Something went wrong";
-    const statusCode = (error instanceof CustomError && error.statusCode) || 500;
+    const { statusCode, message, errors } = classifyError(error);
 
-    res.status(statusCode).json(ErrorResponse(message));
-}
+    res.status(statusCode).json(ErrorResponse(message, errors));
+};
 
 export default errorHandler;
